a27513184efe064c3d5335a8bb899ebe553d092f
xieyunc-dmxy <xieyunc@qq.com>
2019-09-24 10:36:06 +0800

    更改ForDemo.java例子

--- week04/for/ForDemo.java
+++ week04/for/ForDemo.java
@@ -1,11 +1,13 @@
+/*
+	本例子计算从1加到100，如果和达到1000就退出
+*/
 public class ForDemo {
 	public static void main(String[] args) {
-		int i = 3,s = 0;
-		while(i<=100) {
+		int s = 0;
+		for(int i=1;i<=100;i++) {
 			s += i;
-			if (s>=800)
+			if (s>=1000)
 				break;
-			i+=3;
 		}
 		System.out.println("i="+i+",s="+s);
 	}
