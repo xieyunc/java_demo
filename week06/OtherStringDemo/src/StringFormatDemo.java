c84aa51cd4f36567ff35f7a40f3a26311fc8bc15
xieyunc <xieyunc@qq.com>
2019-10-15 11:24:21 +0800

    增加了数组的格式输出！

--- week06/OtherStringDemo/src/StringFormatDemo.java
+++ week06/OtherStringDemo/src/StringFormatDemo.java
@@ -6,9 +6,9 @@ public class StringFormatDemo {
         int x = 97;
         byte ch = 65;
 
-
+        System.out.println("格式化输出：");
         System.out.println(String.format("%s,%-6.2f,%b,%05d,%c",ss,dd,bl,x,x));
 
-        System.out.printf("%s,%-6.2f,%b,%05d,%c,%c",ss,dd,bl,x,x,ch);
+        System.out.printf("%s,%-6.2f,%b,%05d,%c,%d,%c",ss,dd,bl,x,x,ch,ch);
     }
 }
