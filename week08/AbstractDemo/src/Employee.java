557c683b77a91e470bde5458866e05e253b0fc14
xieyunc-dmxy <xieyunc@qq.com>
2019-10-22 11:46:40 +0800

    为抽象类的Demo增加了多态调用方式

--- week08/AbstractDemo/src/Employee.java
+++ week08/AbstractDemo/src/Employee.java
@@ -6,11 +6,16 @@ abstract class Employee {
     String name; //姓名
     String id;   //工号
     double pay;  //薪水
-    Employee(String name,String id,double pay)
-    {
+
+    Employee(String name,String id,double pay) {
         this.name = name;
         this.id = id;
         this.pay = pay;
     }
+
+    void showInfo() {
+        System.out.println(String.format("工号:%s,姓名:%s,薪水:%,.2f",id,name,pay));
+    }
+
     public abstract void work();
 }
