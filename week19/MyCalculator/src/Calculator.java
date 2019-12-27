953f3d63c4b9b7919c8598d10d8d24c71426efd0
xieyunc <xieyunc@qq.com>
2019-12-28 07:39:25 +0800

    这才是那个改进版的计算器

--- week19/MyCalculator/src/Calculator.java
+++ week19/MyCalculator/src/Calculator.java
@@ -1,6 +1,6 @@
 /**
  * 文件名：Calculator.java
- * 功能描述：
+ * 功能描述：简易计算器
  */
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-
+ 
 @SuppressWarnings("serial")
 class CalculatorPanel extends JPanel {
     private Font font = new Font(Font.MONOSPACED,Font.BOLD,14);
@@ -21,14 +21,14 @@ class CalculatorPanel extends JPanel {
     private boolean flag; // 用于判断是否清空显示区域的值,true需要,false不需要
     private String lastCommand; // 用于保存运算符
     JTextField display; // 显示区域
-
+ 
     public CalculatorPanel() {
         // 初始化各项值
         result = 0;
         start = true;
         flag = false;
         lastCommand = "=";
-
+ 
         // 设置CalculatorPanel这个面板的布局为框架布局
         setLayout(new BorderLayout());
         // 这个按钮用来显示值的区域
@@ -37,7 +37,7 @@ class CalculatorPanel extends JPanel {
         //将文本内容设置在文本框的左边
         display.setHorizontalAlignment(display.RIGHT);
         display.setEnabled(false);// 按钮样式设置为禁用样式
-
+ 
         // 用来初始化,清除用的
         JButton clear = new JButton("清除");
         clear.setFont(font);
@@ -52,49 +52,54 @@ class CalculatorPanel extends JPanel {
                 display.setText("0.0");
             }
         });
-
+ 
         jp = new JPanel();
         // 将此面板布局设置为网格布局,4行4列
-        jp.setLayout(new GridLayout(4, 4));
-
+        jp.setLayout(new GridLayout(5, 4));
+ 
         // 实例化监听器对象
-        NumberAction na = new NumberAction();
-        CommandAction ca = new CommandAction();
-
-        makeButton("7", na);
-        makeButton("8", na);
-        makeButton("9", na);
-        makeButton("/", ca);
-
-        makeButton("4", na);
-        makeButton("5", na);
-        makeButton("6", na);
-        makeButton("*", ca);
-
-        makeButton("1", na);
-        makeButton("2", na);
-        makeButton("3", na);
-        makeButton("-", ca);
-
-        makeButton("0", na);
-        makeButton(".", na);
-        makeButton("+", ca);
-        makeButton("=", ca);
-
+        NumberAction numAction = new NumberAction();
+        CommandAction cmdAction = new CommandAction();
+ 
+        makeButton("7", numAction);
+        makeButton("8", numAction);
+        makeButton("9", numAction);
+        makeButton("←", numAction);
+ 
+        makeButton("4", numAction);
+        makeButton("5", numAction);
+        makeButton("6", numAction);
+        makeButton("/", cmdAction);
+ 
+        makeButton("1", numAction);
+        makeButton("2", numAction);
+        makeButton("3", numAction);
+        makeButton("*", cmdAction);
+ 
+        makeButton("0", numAction);
+        makeButton(".", numAction);
+        makeButton("=", cmdAction);
+        makeButton("-", cmdAction);
+ 
+        makeButton("±", cmdAction);
+        makeButton("^", cmdAction);
+        makeButton("%", cmdAction);
+        makeButton("+", cmdAction);
+ 
         // 将面板,两个按键加入到我的面板,实现计算器的界面
         add(display, BorderLayout.NORTH);
         add(jp, BorderLayout.CENTER);
         add(clear, BorderLayout.SOUTH);
-
+ 
     }
-
+ 
     private void makeButton(String buttonName, ActionListener al) {
         JButton jb = new JButton(buttonName);
         jb.setFont(font);
         jp.add(jb);
         jb.addActionListener(al);
     }
-
+ 
     // 数字监听器
     private class NumberAction implements ActionListener {
         @Override
@@ -106,26 +111,41 @@ class CalculatorPanel extends JPanel {
                 if (input.equals(".")) {
                     return;
                 }
+                if (input.equals("←")) {
+                    return;
+                }
                 // 如果是"0.0"的话,就清空
                 if (display.getText().equals("0.0")) {
                     display.setText("");
                 }
                 start = false;// 改变是否首次输入的标记值
             } else {
+                if (input.equals("←")) {
+                    String s = display.getText();
+                    if (Math.abs(Double.valueOf(display.getText()))>0.0) {
+                        s = s.substring(0,s.length()-1);
+                        if (s.equals("")) {
+                            s = "0.0";
+                            start = true;
+                        }
+                        display.setText(s);
+                    }
+                    return;
+                }
                 // 判断显示区域的值里面是否已经有".",如果有,输入的又是".",就什么都不做
                 if (display.getText().indexOf(".") != -1) {
                     if (input.equals(".")) {
                         return;
                     }
                 }
-
+ 
                 // 判断显示区域的值里面只有"-",输入的又是".",就什么都不做
                 if (display.getText().equals("-")) {
                     if (input.equals(".")) {
                         return;
                     }
                 }
-
+ 
                 // 判断显示区域的值如果是"0",输入的不是".",就什么也不做
                 if (display.getText().equals("0")) {
                     if (!input.equals(".")) {
@@ -141,22 +161,28 @@ class CalculatorPanel extends JPanel {
             display.setText(display.getText() + input);// 设置显示区域的值
         }
     }
-
+ 
     // 运算符监听器
     private class CommandAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton jb = (JButton) e.getSource(); // 获取事件源对象
             String inputCommand = jb.getText(); // 获取事件源对象上的标签
-
+ 
             if (start) {// 首次输入
                 // 一上就是"-"
-                if (inputCommand.equals("-")) {
+                if (inputCommand.equals("-") || inputCommand.equals("±")) {
                     display.setText("-");// 显示区域的内容设置为"-"
                     start = false;// 改变首次输入的标记
                 }
             } else {
-
+                if (inputCommand.equals("±")) {
+                    double x = Double.parseDouble(display.getText());
+                    x = -x;
+                    display.setText(String.valueOf(x));
+                    return;
+                }
+ 
                 if (!flag) {// 如果flag=false不需要清空显示区的值,就调用方法计算
                     calculate(Double.parseDouble(display.getText()));// 保存显示区域的值,并计算
                 }
@@ -166,7 +192,7 @@ class CalculatorPanel extends JPanel {
             }
         }
     }
-
+ 
     // 计算用的方法
     private void calculate(double x) {
         if (lastCommand.equals("+")) {
@@ -177,13 +203,19 @@ class CalculatorPanel extends JPanel {
             result *= x;
         } else if (lastCommand.equals("/")) {
             result /= x;
+        } else if (lastCommand.equals("%")) {
+            result %= x;
+        } else if (lastCommand.equals("^")) {
+            result = Math.pow(result,x);
+        } else if (lastCommand.equals("±")) {
+            result = -result;
         } else if (lastCommand.equals("=")) {
             result = x;
         }
         display.setText("" + result);
     }
 }
-
+ 
 class MyFrameUitl {
     public static void init(JFrame jFrame, Dimension frameSize, String title,
                             String iconFileName, boolean resizable) {
@@ -194,15 +226,15 @@ class MyFrameUitl {
         // 获取宽高
         int screenWidth = screenSize.width;
         int screenHeight = screenSize.height;
-
+ 
         // 找中心点
         int centerX = screenWidth / 2;
         int centerY = screenHeight / 2;
-
+ 
         // 解析窗体大小
         int frameWidth = frameSize.width;
         int frameHeight = frameSize.height;
-
+ 
         // 设置窗体位置
         jFrame.setBounds(centerX - frameWidth / 2, centerY - frameHeight / 2,
                 frameWidth, frameHeight);
@@ -210,7 +242,7 @@ class MyFrameUitl {
         if (iconFileName != null) {
             jFrame.setIconImage(tk.getImage(iconFileName));// 设置图标
         }
-
+ 
         // 设置皮肤com.sun.java.swing.plaf.windows.WindowsLookAndFeel
         /**/
         try {
@@ -219,25 +251,21 @@ class MyFrameUitl {
         } catch (Exception e1) {
             e1.printStackTrace();
         }
-
         jFrame.setLocationRelativeTo(null);
         jFrame.setResizable(resizable);// 设置窗体是否可以改变大小
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置程序关闭动作
         jFrame.setVisible(true);// 显示窗体
-
     }
 }
-
 class CalculatorFrame extends JFrame {
     public CalculatorFrame() {
         add(new CalculatorPanel());
-        MyFrameUitl.init(this, new Dimension(800, 600), "简易计算器", null,
+        MyFrameUitl.init(this, new Dimension(800, 600), "简易计算器(xieyunc)", null,
                 false);
         // 让组件按原始大小显示,并且窗体的大小刚好能够容纳所有的组件
         this.pack();
     }
 }
-
 public class Calculator {
     public static void main(String[] args) {
         new CalculatorFrame();
